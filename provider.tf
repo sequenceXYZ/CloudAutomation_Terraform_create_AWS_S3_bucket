ee2758dfa5fe3c86132914f25866036f9cfc09f5
sequenceXYZ <Agnija.Vjakse@gmail.com>
2024-01-31 12:14:47 +0200

    file upload

---
+++ provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-north-1"
+  #access_key = 
+  #secret_key = 
+
+}
